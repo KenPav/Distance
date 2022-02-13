42aefa43229df15f1579b61eef6fdc13a074cf37
KP <65498619+KenPav@users.noreply.github.com>
2022-02-13 17:00:19 -0500

    Add files via upload

--- Distance.js
+++ Distance.js
@@ -6,7 +6,7 @@ var Testing = function(processingInstance) {
         
         // ProgramCodeGoesHere
 
-         var msg= "02.13.22/16:45";       
+         var msg= "02.13.22/17:00";       
 
          var FirstColor = color(0, 0, 0);
          var SecondColor = color(135,10,10);
@@ -181,7 +181,8 @@ var Testing = function(processingInstance) {
             textSize(40);
             textAlign(CENTER);
             fill(FirstColor);
-            text(chkLoc+" "+mouseX+" "+mouseY,600,170)
+            text(chkLoc+" "+mouseX+" "+mouseY,600,120);
+            text(Area.toFixed(0),600,170);
             if (Area===0) {
                 text("Set Point #"+(Count+1),350,170);
                 text(Lat2.toFixed(6)+", "+Long2.toFixed(6),350,250);
